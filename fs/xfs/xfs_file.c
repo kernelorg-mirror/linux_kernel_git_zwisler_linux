99d4d0672af688c71bf112afea4548c861b99ef6
Toshi Kani <toshi.kani@hpe.com>
2016-08-29 13:11:21 -0600

    ext2/4, xfs: call thp_get_unmapped_area() for pmd mappings
    To support DAX pmd mappings with unmodified applications,
    filesystems need to align an mmap address by the pmd size.
    
    Call thp_get_unmapped_area() from f_op->get_unmapped_area.
    
    Note, there is no change in behavior for a non-DAX file.
    
    Signed-off-by: Toshi Kani <toshi.kani@hpe.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Matthew Wilcox <mawilcox@microsoft.com>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Cc: Andreas Dilger <adilger.kernel@dilger.ca>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -1828,6 +1828,7 @@ const struct file_operations xfs_file_operations = {
 	.open		= xfs_file_open,
 	.release	= xfs_file_release,
 	.fsync		= xfs_file_fsync,
+	.get_unmapped_area = thp_get_unmapped_area,
 	.fallocate	= xfs_file_fallocate,
 	.copy_file_range = xfs_file_copy_range,
 	.clone_file_range = xfs_file_clone_range,
