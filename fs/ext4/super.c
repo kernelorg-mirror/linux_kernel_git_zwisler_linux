e768bbdcc76b7ca30060c435386c289b6c40c09b
Jan Kara <jack@suse.cz>
2016-10-06 16:06:06 +0200

    ext4: Let S_DAX set only if DAX is really supported
    Currently we have S_DAX set inode->i_flags for a regular file whenever
    ext4 is mounted with dax mount option. However in some cases we cannot
    really do DAX - e.g. when inode is marked to use data journalling, when
    inode data is being encrypted, or when inode is stored inline. Make sure
    S_DAX flag is appropriately set/cleared in these cases.
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1125,6 +1125,10 @@ static int ext4_set_context(struct inode *inode, const void *ctx, size_t len,
 			ext4_set_inode_flag(inode, EXT4_INODE_ENCRYPT);
 			ext4_clear_inode_state(inode,
 					EXT4_STATE_MAY_INLINE_DATA);
+			/*
+			 * Update inode->i_flags - e.g. S_DAX may get disabled
+			 */
+			ext4_set_inode_flags(inode);
 		}
 		return res;
 	}
@@ -1139,6 +1143,7 @@ static int ext4_set_context(struct inode *inode, const void *ctx, size_t len,
 			len, 0);
 	if (!res) {
 		ext4_set_inode_flag(inode, EXT4_INODE_ENCRYPT);
+		/* Update inode->i_flags - e.g. S_DAX may get disabled */
 		res = ext4_mark_inode_dirty(handle, inode);
 		if (res)
 			EXT4_ERROR_INODE(inode, "Failed to mark inode dirty");
