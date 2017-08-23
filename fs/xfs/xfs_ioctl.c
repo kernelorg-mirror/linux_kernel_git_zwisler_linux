601660ed44ac3e418ed02a88f9494fbf46688381
Ross Zwisler <ross.zwisler@linux.intel.com>
2017-08-23 12:43:42 -0600

    xfs: always use DAX if mount option is used
    The current code has an issue where the user can't reliably tell whether or
    not DAX is being used to service page faults and I/O when the DAX mount
    option is used.  In this case each inode within the mounted filesystem
    starts with S_DAX set due to the mount option, but it can be cleared if
    someone touches the individual inode flag.
    
    For example:
    
      # mount | grep dax
      /dev/pmem0 on /mnt type xfs
      (rw,relatime,seclabel,attr2,dax,inode64,sunit=4096,swidth=4096,noquota)
    
      # touch /mnt/a /mnt/b   # both files currently use DAX
    
      # xfs_io -c "lsattr" /mnt/*  # neither has the DAX inode option set
      ----------e----- /mnt/a
      ----------e----- /mnt/b
    
      # xfs_io -c "chattr -x" /mnt/a  # this clears S_DAX for /mnt/a
    
      # xfs_io -c "lsattr" /mnt/*
      ----------e----- /mnt/a
      ----------e----- /mnt/b
    
    We end up with both /mnt/a and /mnt/b looking identical from the point of
    view of the mount option and from lsattr, but one is using DAX and the
    other is not.
    
    Fix this by always doing DAX I/O when either the mount option is set or
    when the DAX inode flag is set.  This means that DAX will always be used
    for all inodes on a filesystem mounted with -o dax, making the usage
    reliable and detectable.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -1008,7 +1008,7 @@ xfs_diflags_to_linux(
 		inode->i_flags |= S_NOATIME;
 	else
 		inode->i_flags &= ~S_NOATIME;
-	if (xflags & FS_XFLAG_DAX)
+	if ((xflags & FS_XFLAG_DAX) || (ip->i_mount->m_flags & XFS_MOUNT_DAX))
 		inode->i_flags |= S_DAX;
 	else
 		inode->i_flags &= ~S_DAX;
@@ -1091,7 +1091,14 @@ xfs_ioctl_setattr_dax_invalidate(
 			return -EINVAL;
 	}
 
-	/* If the DAX state is not changing, we have nothing to do here. */
+	/*
+	 * If the DAX state is not changing, we have nothing to do here.  If
+	 * the DAX mount option was used we will update the DAX inode flag as
+	 * the user requested but we will continue to use DAX for I/O and page
+	 * faults regardless of how the inode flag is set.
+	 */
+	if (ip->i_mount->m_flags & XFS_MOUNT_DAX)
+		return 0;
 	if ((fa->fsx_xflags & FS_XFLAG_DAX) && IS_DAX(inode))
 		return 0;
 	if (!(fa->fsx_xflags & FS_XFLAG_DAX) && !IS_DAX(inode))
